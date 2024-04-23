d9eee2a0d768a1a595aacf090bbf15261675655c
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-04-23 12:26:24 +0200

    Add files via upload

---
+++ Web Preglednik.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebPreglednik1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Width = this.ClientSize.Width - 20;
+            webBrowser1.Height = this.ClientSize.Height - 80;
+        }
+        private void newTabButton_Click(object sender, EventArgs e)
+        {
+            // Kreirajte novi tab
+            // Kreirajte novu instancu WebBrowser kontrole
+            // Smjestite tu instancu na stvoreni tab
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string url = textBox1.Text;
+                // Provjera da li url počinje protokolov (htttp://, https://)
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = "http://" + url;
+                }
+                webBrowser1.Navigate(url);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
